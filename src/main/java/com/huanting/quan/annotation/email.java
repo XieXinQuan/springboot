7814a6056809b931946d5fdfc8d865253c1748c9
Quan0502 <1372862842@qq.com>
2020-01-22 21:48:27 +0800

    提交一些类

---
+++ src/main/java/com/huanting/quan/annotation/email.java
@@ -0,0 +1,25 @@
+package com.huanting.quan.annotation;
+
+
+import org.hibernate.validator.internal.constraintvalidators.bv.EmailValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * @author: xiexinquan520@163.com
+ * User: XieXinQuan
+ * DATE:2020/1/22
+ */
+@Constraint(validatedBy = EmailValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface email {
+    String message() default "邮箱格式不正确";
+
+    Class<?>[] group() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
