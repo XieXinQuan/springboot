88d68ab513b54e277995dc7c51def872ee2a81a1
Quan0502 <1372862842@qq.com>
2020-03-05 21:16:57 +0800

    提交一些类

--- src/main/java/com/huanting/quan/controller/MessageController.java
+++ src/main/java/com/huanting/quan/controller/MessageController.java
@@ -1,6 +1,10 @@
 package com.huanting.quan.controller;
 
+import com.huanting.quan.Enum.ResultEnum;
+import com.huanting.quan.exception.GlobalException;
+import com.huanting.quan.service.ActivitiService;
 import com.huanting.quan.service.MessageService;
+import com.huanting.quan.service.UserService;
 import com.huanting.quan.service.WebSocketServer;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.validation.constraints.NotBlank;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author: xiexinquan520@163.com
@@ -23,8 +30,16 @@ import java.io.IOException;
 public class MessageController {
     @Resource
     MessageService messageService;
+    @Resource
+    UserService userService;
+    @Resource
+    ActivitiService activitiService;
 
     private Logger logger = LoggerFactory.getLogger(MessageController.class);
+
+    /**
+     * 舍弃
+     */
     @RequestMapping("/send/{toUserId}")
     public String sendToUserId(String message, @PathVariable String toUserId) throws IOException {
 
@@ -38,28 +53,45 @@ public class MessageController {
 
     @ApiOperation("发送消息")
     @PostMapping("/send")
-    public String send(@NotBlank(message = "消息内容不能为空") @RequestParam("content") String content,
+    public Object send(@NotBlank(message = "消息内容不能为空") @RequestParam("content") String content,
                        @NotBlank(message = "对方Id为空") @RequestParam("toUserId") Long toUserId) throws IOException {
-
-        messageService.sendMessage(toUserId, content);
-
-        return "发送成功";
+        return messageService.sendMessage(toUserId, content, userService.isMember());
     }
 
     @ApiOperation("登录加载所有通知")
     @PostMapping("/loadAllNotice")
     public Object loadAllNotice(){
+        List<Object> result = new ArrayList<>();
+
+        //加载好友请求
+        List<Map<String, Object>> list = messageService.loadAllAddFriendMessage();
+
+        //加载需要我审批的申请
+        List<Map<String, Object>> list1 = activitiService.loadNeedMyApproveTask();
 
 
-        return messageService.loadAllAddFriendMessage();
+        //合并所有
+        if (list != null) {
+            result.addAll(list);
+        }
+        if (list1 != null) {
+            result.addAll(list1);
+        }
+
+        return result;
 
     }
 
     @ApiOperation("撤回消息")
     @PostMapping("/withdraw")
-    public String withdraw(@RequestParam("id") Long id){
+    public String withdraw(@RequestParam("receiveUserId") Long receiveUserId,
+                           @RequestParam("time") Long time, @RequestParam("content") String content) throws IOException {
 
-        return messageService.withdraw(id);
+        boolean isMember = userService.isMember();
+        if (!isMember){
+            throw new GlobalException(ResultEnum.ApplicationMember.getKey(), "只有会员才能撤回消息, 请先注册会员..");
+        }
+        return messageService.withdraw(receiveUserId, content, time);
     }
 
 }
